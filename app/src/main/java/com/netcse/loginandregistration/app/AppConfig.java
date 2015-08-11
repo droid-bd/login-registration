081424d4dc2dd7a4332ea7b44f22deb59599f068
netcse <smamran09@gmail.com>
2015-08-11 15:51:27 +0600

    Initial commit

---
+++ app/src/main/java/com/netcse/loginandregistration/app/AppConfig.java
@@ -0,0 +1,9 @@
+package com.netcse.loginandregistration.app;
+ 
+public class AppConfig {
+    // Server user login url
+    public static String URL_LOGIN = "http://192.168.1.7/android_login_api/";
+ 
+    // Server user register url
+    public static String URL_REGISTER = "http://192.168.1.7/android_login_api/";
+}
